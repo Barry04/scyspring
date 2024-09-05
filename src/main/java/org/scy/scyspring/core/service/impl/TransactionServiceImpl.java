d75b54894fab51fa24c2d1229c41d631542b1c74
suncy40801 <suncy40801@hundusn.com>
2024-09-05 08:54:40 +0800

    refactor(core): 使用Java事务管理器优化事务处理
    优化了TransactionServiceImpl和TransactionServiceTwoImpl中的事务处理，使用了Java事务管理器替代编程式事务。此更改增强了事务的鲁棒性和可管理性，简化了事务的处理逻辑。

--- src/main/java/org/scy/scyspring/core/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/scy/scyspring/core/service/impl/TransactionServiceImpl.java
@@ -7,8 +7,11 @@ import org.scy.scyspring.core.service.AsyncService;
 import org.scy.scyspring.core.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -27,18 +30,46 @@ public class TransactionServiceImpl {
     @Resource
     private TransactionServiceTwoImpl transactionServiceTwo;
 
+    @Resource
+    private PlatformTransactionManager transactionManager;
+
+
+    /**
+     * 编译方法，用于处理与用户信息相关的业务逻辑
+     * 该方法通过事务处理来确保每个用户的业务操作的原子性
+     */
     public void compile() {
+        // 定义事务
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        // 设置事务传播行为，表示总是新建事务执行代码块，如果有异常则回滚事务
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        // 获取所有用户信息列表
         List<UserInfo> list = userInfoService.list();
+        // 遍历用户信息列表
         for (UserInfo userInfo : list) {
+            // 开始事务
+            TransactionStatus status = transactionManager.getTransaction(def);
             try {
+                // 执行具体的业务逻辑
                 extracted(userInfo);
+                // 提交事务，表示操作成功
+                transactionManager.commit(status);
             } catch (Exception e) {
+                // 记录错误日志
                 log.error("error msg : {}", e.getMessage(), e);
+                // 回滚事务，表示操作失败
+                transactionManager.rollback(status);
             }
         }
     }
 
-    @Transactional(propagation = Propagation.NESTED)
+    /**
+     * 具体的业务处理方法
+     * 该方法用于处理单个用户的信息
+     *
+     * @param userInfo 用户信息对象，包含用户的具体数据
+     */
     public void extracted(UserInfo userInfo) {
         transactionServiceTwo.compile(userInfo);
     }
--- src/main/java/org/scy/scyspring/core/service/impl/TransactionServiceTwoImpl.java
+++ src/main/java/org/scy/scyspring/core/service/impl/TransactionServiceTwoImpl.java
@@ -4,13 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.scy.scyspring.core.domain.UserInfo;
 import org.scy.scyspring.core.service.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.annotation.Resource;
 
@@ -22,31 +17,20 @@ public class TransactionServiceTwoImpl {
     @Resource
     private UserInfoService userInfoService;
 
-    @Autowired
-    private PlatformTransactionManager transactionManager;
 
     public void compile(UserInfo userInfo) {
-        // 定义事务
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW); // 设置事务传播行为
 
-        // 开始事务
-        TransactionStatus status = transactionManager.getTransaction(def);
-        try {
-            LambdaUpdateWrapper<UserInfo> updateWrapper = new LambdaUpdateWrapper<>();
-            if (userInfo.getUsername().contains("J")) {
-                updateWrapper.set(UserInfo::getAge, 88);
-                updateWrapper.eq(UserInfo::getUuid, userInfo.getUuid());
-                userInfoService.update(updateWrapper);
-                throw new RuntimeException("my asyncAloneTransaction Error");
-            } else {
-                updateWrapper.set(UserInfo::getAge, 99);
-                updateWrapper.eq(UserInfo::getUuid, userInfo.getUuid());
-                userInfoService.update(updateWrapper);
-            }
-        } catch (Exception e) {
-            // 事务回滚
-            transactionManager.rollback(status);
+        LambdaUpdateWrapper<UserInfo> updateWrapper = new LambdaUpdateWrapper<>();
+        if (userInfo.getUsername().contains("J")) {
+            updateWrapper.set(UserInfo::getAge, 88);
+            updateWrapper.eq(UserInfo::getUuid, userInfo.getUuid());
+            userInfoService.update(updateWrapper);
+            throw new RuntimeException("my asyncAloneTransaction Error");
+        } else {
+            updateWrapper.set(UserInfo::getAge, 66);
+            updateWrapper.eq(UserInfo::getUuid, userInfo.getUuid());
+            userInfoService.update(updateWrapper);
         }
+
     }
 }
